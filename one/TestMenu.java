2131d038ba34b25348f3865711a880e21ab23826
curryWu <814530407@qq.com>
2019-07-22 11:16:52 +0800

    java文件

---
+++ one/TestMenu.java
@@ -0,0 +1,33 @@
+package cn.jbit.classandobject;
+import java.util.Scanner;
+/**
+ * TestMenu.java 
+ */
+
+public class TestMenu {
+
+	/**
+	 * 系统程序入口
+	 */
+	public static void main(String[] args) {        
+        
+		boolean con=true;
+		do{
+		 /*显示登录菜单*/
+		Menu menu = new Menu();
+		menu.showLoginMenu();
+		
+		/*实现菜单*/
+		Scanner input = new Scanner(System.in);
+        int choice = input.nextInt();
+        switch(choice){
+        	case 1:          
+        		menu.showMainMenu();           
+        		break;
+        	case 2: 
+        		System.out.println("谢谢您的使用！");
+        		con=false;
+       }
+	}while(con);
+	}
+}
